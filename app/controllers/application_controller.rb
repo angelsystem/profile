8ecb3ec5c79f6b0e32e394bc5b7ba715f6001f75
angelsystem <angelsystem1387@gmail.com>
2015-05-22 17:16:53 -0500

    fix bug detect language browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,15 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  # def set_locale
+  #   # logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+  #   I18n.locale = extract_locale_from_accept_language_header
+  #   logger.debug "* Locale set to '#{I18n.locale}'"
+  # end
+
   def set_locale
-    # logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-    I18n.locale = extract_locale_from_accept_language_header
-    # logger.debug "* Locale set to '#{I18n.locale}'"
+    I18n.locale = params[:locale] || I18n.default_locale
+    # I18n.locale = extract_locale_from_accept_language_header
   end
 
   def default_url_options(options={})
@@ -19,4 +24,5 @@ class ApplicationController < ActionController::Base
     def extract_locale_from_accept_language_header
       request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
     end
+
 end
